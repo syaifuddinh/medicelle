4efc33b0d0c0d44b85593217faa6c7fb049e97ac
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2020-07-13 14:34:51 +0800

    Fixing assesment in medical record migration

--- database/migrations/2020_07_13_025310_assestment_detail_id_in_medical_record_details.php
+++ database/migrations/2020_07_13_025310_assestment_detail_id_in_medical_record_details.php
@@ -14,8 +14,11 @@ class AssestmentDetailIdInMedicalRecordDetails extends Migration
     public function up()
     {
         Schema::table('medical_record_details', function (Blueprint $table) {
-            $table->unsignedInteger('medical_record_details')
-            ->where
+            $table->unsignedInteger('assesment_detail_id')
+            ->nullable(true)
+            ->index();
+
+            $table->foreign('assesment_detail_id')->references('id')->on('assesment_details')->onDelete('set null');
         });
     }
 
@@ -27,7 +30,7 @@ class AssestmentDetailIdInMedicalRecordDetails extends Migration
     public function down()
     {
         Schema::table('medical_record_details', function (Blueprint $table) {
-            //
+            $table->dropColumn(['assesment_detail_id']);
         });
     }
 }
