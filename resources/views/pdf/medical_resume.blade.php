23858a5380b16fd2e2047f9a8baf01cb7ff1557a
Afid Kurniawan <afidkurniawan@gmail.com>
2021-01-19 04:17:35 -0500

    Menampilkan diagnostik lab yg di checklist, menampilkan diagnosis, dan merapikan resume medis

--- resources/views/pdf/medical_resume.blade.php
+++ resources/views/pdf/medical_resume.blade.php
@@ -83,6 +83,32 @@
                 <p>Pemeriksaan fisik : {{ $medicalRecord->physique }}</p>
                 <br>
                 <div style='margin-bottom:2mm'>
+                    <p>Pemeriksaan tambahan :</p>
+                    <?php if($medicalRecord->diagnostic) { ?>
+                    <ol style='margin-top:3mm;margin-left:6mm'>
+                      @foreach($medicalRecord->diagnostic as $unit4)
+                            <li>{{ $unit4->item->name ?? '' }} 
+				<?php if($unit4->laboratory_pivot) { ?> (
+				@foreach($unit4->laboratory_pivot["additional"]->treatment[0]->detail as $detdiag)
+				<? if(array_key_exists("is_active",$detdiag)) {?>
+				{{ $detdiag->name }},
+				<? }?>
+                       	@endforeach )
+				<?php }?>
+                            </li>
+                       @endforeach
+                    </ol><br>
+                    <?php } else { ?>
+                    <p>(tidak ada diagnostik yang diminta)</p>
+                    <?php } ?>
+                    <p style='margin-top:2mm'>Diagnosis :</p>
+                    <ol style='margin-top:3mm;margin-left:6mm'>
+                     @foreach($medicalRecord->diagnose_history as $unit)
+                        <li>{{ $unit->disease->name ?? $unit->additional->diagnose_name }} (ket : {{$unit->description}})
+                        </li>
+                     @endforeach
+                    </ol>
+                    <br>
                     <p>Terapi (Obat) :</p>
                     <?php if($medicalRecord->drug) { ?>
                     <ol style='margin-top:3mm;margin-left:6mm'>
@@ -92,7 +118,7 @@
                     </ol><br>
                     <?php } else { ?>
                     <p>(tidak ada obat yang diberikan)</p>
-                    <?php } ?>
+                    <?php } ?><!--
                     <p>Terapi (BHP) :</p>
                     <?php if($medicalRecord->bhp) { ?>
                     <ol style='margin-top:3mm;margin-left:6mm'>
@@ -102,7 +128,7 @@
                     </ol><br>
                     <?php } else { ?>
                     <p>(tidak ada BHP yang diberikan)</p>
-                    <?php } ?>
+                    <?php } ?> -->
                     <p>Terapi (Tindakan) :</p>
                     <?php if($medicalRecord->treatment) { ?>
                     <ol style='margin-top:3mm;margin-left:6mm'>
@@ -112,16 +138,6 @@
                     </ol><br>
                     <?php } else { ?>
                     <p>(tidak ada tindakan yang dilakukan)</p>
-                    <?php } ?>
-                    <p>Terapi (Diagnostic) :</p>
-                    <?php if($medicalRecord->diagnostic) { ?>
-                    <ol style='margin-top:3mm;margin-left:6mm'>
-                      @foreach($medicalRecord->diagnostic as $unit4)
-                            <li>{{ $unit4->item->name ?? '' }}</li>
-                       @endforeach
-                    </ol><br>
-                    <?php } else { ?>
-                    <p>(tidak ada diagnostik yang diminta)</p>
                     <?php } ?>
                     <p>Terapi (Paket Tindakan) :</p>
                     <?php if($medicalRecord->treatment_group) { ?>
@@ -133,13 +149,7 @@
                     <?php } else { ?>
                     <p>(tidak ada paket tindakan yang dilakukan)</p>
                     <?php } ?>
-                    <p style='margin-top:2mm'>Diagnosis :</p>
-                    <ol style='margin-top:3mm;margin-left:6mm'>
-                     @foreach($medicalRecord->diagnose_history as $unit)
-                        <li>{{ $unit->disease->name ?? '' }}</li>
-                     @endforeach
-                    </ol>
-                    <br>
+
                     <p>{{ $medicalRecord->usg ?? '' }}</p>
                     <br>
                     <p style='margin-top:3mm'>Jadwal kontrol selanjutnya pada hari {{ $medicalRecord->next_schedule->date ? Mod::day($medicalRecord->next_schedule->date) : $shortDot }}, tanggal {{ $medicalRecord->next_schedule->date ? Mod::fullDate($medicalRecord->next_schedule->date) : $shortDot }}</p>
