32c8d56f28641d20f791e3108b0d613b7b2ce223
Afid Kurniawan <afidkurniawan@gmail.com>
2021-02-09 02:10:11 -0500

    Menampilkan copy nota/kwitansi untuk pembayaran admin&konsul

--- resources/views/pdf/letter_footer.blade.php
+++ resources/views/pdf/letter_footer.blade.php
@@ -1,18 +1,17 @@
 <p style='position: absolute;bottom:10mm;width:100%;'>
-    <span style="display:inline-block;margin-right:22mm">
+    <span style="display:inline-block;margin-right:8mm">
         <img src="{!! asset('images/telephone.png') !!}" style='height:2.5mm;width:auto'>
          {!! $company->phone_number ?? '' !!}
      </span>
-
-    <span style="display:inline-block;margin-right:22mm">
+    <span style="display:inline-block;margin-right:12mm">
         <img src="{!! asset('images/telephone.png') !!}" style='height:2.5mm;width:auto'>
          {!! $company->whatsapp_number ?? '' !!}</span>
          
-    <span style="display:inline-block;margin-right:22mm">
+    <span style="display:inline-block;margin-right:12mm">
         <img src="{!! asset('images/earth.png') !!}" style='height:2.5mm;width:auto'>
         {!! $company->website ?? '' !!}
     </span>
-    <span style="display:inline-block;">
+    <span style="display:inline-block;margin-right:12mm">
         <img src="{!! asset('images/envelope.png') !!}" style='height:2.5mm;width:auto'>
         {!! $company->email ?? '' !!}
     </span>
