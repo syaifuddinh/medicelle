913c3eecb9eaf960c86244881a9ac712bc9df739
Afid Kurniawan <afidkurniawan@gmail.com>
2021-09-15 17:07:43 +0700

    perapian surat keterangan sehat

--- resources/views/pdf/keterangan_sehat.blade.php
+++ resources/views/pdf/keterangan_sehat.blade.php
@@ -18,31 +18,35 @@
         <div class="row" style='margin-top:4mm;'>
             <div style='margin-bottom:6mm'>
                 <div style='display:inline-block'>
-                    <img src="{!! $company->logo !!}" style='width:auto;height:20mm;' alt="">
-                    <p style='font-size:85%'>{!! $company->address !!}</p>
+                    <img src="{!! $company->logo2 !!}" style='width:auto;height:20mm;' alt="">
+                </div>
+                <div style='display:inline-block;padding-bottom:3mm;padding-left:2mm;width:120mm'>
+                    <b style='font-size:106%;text-transform:uppercase'>{!! $company->name !!}</b>
+                    <p>{!! $company->address !!}</p>
+                    <p>Telp : {!! $company->phone_number !!} Fax : {!! $company->fax !!}</p>
                 </div>
             </div>
 
-            <div class="col-md-12" style='padding-left:5.5mm'>
+            <div class="col-md-12" style='padding-left:5.5mm;line-height:8mm;'>
                 <div class="header" style='text-align:center'>
                     <h3 style='font-weight:bold;'><span style='border-bottom:1px solid black'>SURAT KETERANGAN SEHAT</span></h3>
                     <h4 style='font-weight:bold;'>{!! $letter->code !!}</h4>
                 </div>
                 <br>
 
-                <p style="text-indent:8mm">Yang bertanda tangan dibawah ini, dokter {!! $company->name !!} menerangkan dengan sesungguhnya bahwa bahwa : </p><br>
-                <p style='padding-left:8mm'><span style='display:inline-block;width:40mm'>Nama</span> <span style='display:inline-block;font-weight:bold'>: {!! $letter->medical_record->patient->patient_type . ' ' . $letter->medical_record->patient->name !!}</span></p>
+                <p>Yang bertanda tangan dibawah ini, dokter {!! $company->name !!} menerangkan dengan sesungguhnya bahwa : </p><br>
+                <p style='padding-left:8mm'><span style='display:inline-block;width:60mm'>Nama</span> <span style='display:inline-block;font-weight:bold'>: {!! $letter->medical_record->patient->patient_type . ' ' . $letter->medical_record->patient->name !!}</span></p>
 
-                <p style='padding-left:8mm'><span style='display:inline-block;width:40mm'>Alamat</span> <span style='display:inline-block;'>: {!! $letter->medical_record->patient->address !!} </span></p>
+                <p style='padding-left:8mm'><span style='display:inline-block;width:60mm'>Alamat</span> <span style='display:inline-block;width:140mm;'>: {!! $letter->medical_record->patient->address !!} </span></p>
 
-                <p style='padding-left:8mm'><span style='display:inline-block;width:40mm'>Jenis kelamin</span> <span style='display:inline-block;'>: {!! $letter->medical_record->patient->gender !!} </span></p>
+                <p style='padding-left:8mm'><span style='display:inline-block;width:60mm'>Jenis kelamin</span> <span style='display:inline-block;width:60mm;'>: {!! $letter->medical_record->patient->gender !!} </span></p>
 
-                <p style='padding-left:8mm'><span style='display:inline-block;width:40mm'>Tanggal lahir, umur</span> <span style='display:inline-block;'>: {!! Mod::fullDate($letter->medical_record->patient->birth_date) . ', ' . $letter->medical_record->patient->age .  ' Tahun'!!} </span></p>
+                <p style='padding-left:8mm'><span style='display:inline-block;width:60mm'>Tanggal lahir, umur</span> <span style='display:inline-block;width:60mm;'>: {!! Mod::fullDate($letter->medical_record->patient->birth_date) . ', ' . $letter->medical_record->patient->age .  ' Tahun'!!} </span></p>
 
 
-                <p style='padding-left:8mm'><span style='display:inline-block;width:40mm'>Pekerjaan</span> <span style='display:inline-block;'>: {!! $letter->medical_record->patient->job !!} </span></p><br>
+                <p style='padding-left:8mm'><span style='display:inline-block;width:60mm'>Pekerjaan</span> <span style='display:inline-block;'>: {!! $letter->medical_record->patient->job !!} </span></p><br>
 
-                <p style="text-indent:8mm">Hasil pemeriksaan :</p><br>
+                <p>Hasil pemeriksaan :</p><br>
                 <p style='padding-left:8mm'>
                     <span style="display:inline-block;width:30mm">TD : {!! $letter->additional->td !!}</span>
                     <span style="display:inline-block;width:30mm">N : {!! $letter->additional->n !!}</span>
@@ -52,7 +56,7 @@
                 <p style='padding-left:8mm'>
                     <span style="display:inline-block;width:30mm">BB : {!! $letter->additional->bb !!}</span>
                     <span style="display:inline-block;width:30mm">TB : {!! $letter->additional->tb !!}</span>
-                    <span style="display:inline-block;width:30mm">Buta warna : {!! strtolower($letter->additional->buta_warna) !!}</span>
+                    <span style="display:inline-block;width:50mm">Buta warna : {!! strtolower($letter->additional->buta_warna) !!}</span>
                 </p>
                 <p style='padding-left:8mm'>
                     Pemeriksaan mata : {!! $letter->additional->eye_diagnose !!} 
@@ -60,7 +64,7 @@
                 <p style='padding-left:8mm'>
                     Pemeriksaan telinga : {!! $letter->additional->ear_diagnose !!} 
                 </p><br>
-                <p style="text-indent:8mm">Benar telah diperiksa dengan teliti dan pasien dinyatakan dalam keadaan {!! strtolower($letter->option) !!}. Surat keterangan ini dipergunakan untuk {!! $letter->description !!}</p><br><br>
+                <p style="style='padding-left:8mm'">Benar telah diperiksa dengan teliti dan pasien dinyatakan dalam keadaan {!! strtolower($letter->option) !!}. Surat keterangan ini dipergunakan untuk {!! $letter->description !!}</p><br><br>
                 <p>
                     <span style='float:right;text-align:center;display:inline-block;display:inline-block'>
                         {!! $company->city . ', '. Mod::fullDate(date('Y-m-d'))  !!}<br>
